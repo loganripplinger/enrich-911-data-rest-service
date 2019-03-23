f80a3126fa11258928c9a6f114406dc122279cb9
Logan Ripplinger <logan.ripplinger@gmail.com>
2019-03-23 12:47:48 -0700

    Serve incident data

--- src/server/routes/incident-router.js
+++ src/server/routes/incident-router.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
 router.get("/:incidentId", function(req, res, next) {
   const incidentId = req.params.incidentId;
-  res.json({ got: incidentId });
+
+  // Async load incident file
+  const incidentFile = path.join(__dirname, `../data/${incidentId}.json`);
+
+  fs.readFile(incidentFile, (err, data) => {
+    if (err) {
+      res.sendStatus(404);
+      throw err;
+    }
+
+    const incident = JSON.parse(data);
+
+    res.json(incident);
+  });
 });
 
 module.exports = router;
